7a9d958d4ab1e03242074cb4bd479638531bd341
khawlittaa <khawlahafsiaa@gmail.com>
2026-02-26 21:12:27 +0100

    set up xcode project

---
+++ PlacesWiki/PlacesWiki/PlacesWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesWikiApp.swift
+//  PlacesWiki
+//
+//  Created by khaoula hafsia on 26/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct PlacesWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlacesWiki/PlacesWikiTests/PlacesWikiTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesWikiTests.swift
+//  PlacesWikiTests
+//
+//  Created by khaoula hafsia on 26/02/2026.
+//
+
+import Testing
+@testable import PlacesWiki
+
+struct PlacesWikiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
